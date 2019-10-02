82c08b59746812f30ca7fa492144675fdc2f2154
JiangHaoyang <897280553@qq.com>
2019-10-02 19:53:29 +0800

    Add files via upload

--- toyCode/test_1.py
+++ toyCode/test_1.py
@@ -1,28 +1,15 @@
-import torchvision.models as models
-import torch
-import torchvision
-import torch.nn as nn
 import numpy as np
+import torch
+import PIL
+from PIL import Image
 import torchvision.transforms as transforms
-from torch.autograd import Variable
 
-#download model
-ResNet18 = models.resnet18(pretrained=True)
-ResNet18.eval()
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+imag = Image.fromarray(a.astype('uint8'))
 
-#define how an PIL image transforms into the network input
-inputTransformer=transforms.Compose([transforms.Resize(256),
-                                    transforms.CenterCrop(224),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])])
+toPIL = transforms.ToPILImage()
+toTensor = transforms.ToTensor()
 
-#download data
-imageData_train = torchvision.datasets.ImageNet(root='./ImageData/imageNet', train = True, transform = inputTransformer, download=True) #download training data set
-imageData_test = torchvision.datasets.ImageNet(root='./ImageData/imageNet', train = False, transform = inputTransformer, download=True) #download testing data set
-
-# image,label = imageData_train[0]
-# predict = ResNet18.forward(Variable(image[None,:,:,:],requires_grad = True))
-
-# print(predict.shape)
-
-#print(output)
+print(np.array(imag).max())
+print(toTensor(imag).max())
+print(9/255)
